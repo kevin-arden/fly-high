d84b574728bd7f5649c6b4ddc58d8a5ac437fdb2
Kevin Arden <kevin.arden@tsmid.com>
2022-10-05 01:39:10 +0700

    initial commit

---
+++ components/Header.tsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import Image from 'next/image'
+import Link from 'next/link'
+import style from "../styles/Header.module.sass"
+
+const Header = () => {
+  return (
+    <div className={style.container}>
+      <div className={style.logo}>
+        <Link href="/">
+          <div>
+        <Image src="/main-logo.svg" height={35} width={50} alt="none" /> Fly High   
+          </div>
+        </Link>
+      </div>
+      <div className={style.link_container}>
+        <Link href="/tickets">
+
+          <span className={style.link}>Find Ticket</span> 
+
+        </Link>
+        <Link className={style.link} href="/booking">
+          <span className={style.link}>My Booking</span> 
+          </Link>
+      </div>
+      <div>
+        <button className={style.button}>Sign Up</button>
+      </div>
+    </div>
+  )
+}
+
+export { Header}
